8d851da8ad65daf8a8a7f645eb67d991865094cf
WeAthFolD <weathfold@li-dev.cn>
2019-01-26 21:06:07 +0800

    Update

---
+++ Assets/Content/Script/LevelScroll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScroll : MonoBehaviour
+{
+    public float scrollSpeed = 3.0f;
+
+    void Update()
+    {
+        transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
+    }
+}
