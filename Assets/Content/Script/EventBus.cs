c86428de7dca8f8191f92b9acbe57f268d130796
WeAthFolD <weathfold@li-dev.cn>
2019-01-26 10:35:04 +0800

    Add EventBus

---
+++ Assets/Content/Script/EventBus.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventBus {
+
+	public delegate void EventHandler<T>(T ev);
+
+	static readonly Dictionary<Type, List<object>> eventHandlers = new Dictionary<Type, List<object>>();
+
+	public static void Subscribe<T>(EventHandler<T> handler) {
+		var type = typeof(T);
+		if (!eventHandlers.ContainsKey(type)) {
+			eventHandlers.Add(type, new List<object>());
+		}
+
+		eventHandlers[type].Add(handler);
+	}
+
+	public static void Unsubscribe<T>(EventHandler<T> handler) {
+		var type = typeof(T);
+		if (eventHandlers.ContainsKey(type)) {
+			eventHandlers[type].Remove(handler);
+		}
+	}
+
+	public static void Post<T>(T ev) {
+		List<object> list;
+		eventHandlers.TryGetValue(typeof(T), out list);
+		if (list != null && list.Count > 0) {
+			foreach (var _handler in list) {
+				((EventHandler<T>) _handler) (ev);
+			}
+		} else {
+			Debug.LogWarning(ev + " Has no receiver");
+		}
+	}
+
+}
