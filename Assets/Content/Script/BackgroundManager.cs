fec01410d29d2f32d9bbc498e4aa73e6e1bd9692
FalseMan <maxinghua123000@gmail.com>
2019-01-26 17:58:47 +0800

    no message

--- Assets/Content/Script/BackgroundManager.cs
+++ Assets/Content/Script/BackgroundManager.cs
@@ -8,12 +8,32 @@ public class BackgroundManager : MonoBehaviour
 	private float _scrollSpeed;
 
 	[SerializeField]
-	private Transform _background;
+	private Transform[] _backgrounds;
+
+	[SerializeField]
+	private Vector3 _scrollUpdate = new Vector3(21.5f, 0, 0);
+
+	[SerializeField]
+	private Vector3 _updatePoint = new Vector3(-23f, 0, 0);
+
 
 	private void Update()
 	{
 		float scrollUpdate = _scrollSpeed * Time.deltaTime;
-		_background.Translate(new Vector3(_scrollSpeed, 0, 0));
+		transform.Translate(new Vector3(_scrollSpeed, 0, 0));
+		UpdateBGScroll();
+	}
+
+	private void UpdateBGScroll()
+	{
+		foreach (Transform bgTrans in _backgrounds)
+		{
+			if (bgTrans.position.x <= _updatePoint.x)
+			{
+				bgTrans.Translate(bgTrans.position + _scrollUpdate);
+			}
+		}
+		return;
 	}
 
 }
