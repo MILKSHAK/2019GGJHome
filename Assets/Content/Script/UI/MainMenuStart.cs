0b2e8f00466677af5d369c34bcd6d7ee20d92ad2
WeAthFolD <weathfold@li-dev.cn>
2019-01-27 09:49:57 +0800

    Entry

---
+++ Assets/Content/Script/UI/MainMenuStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuStart : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1.0f);
+        yield return new WaitUntil(() => Input.anyKeyDown);
+        
+        SceneManager.LoadScene("PreScene");
+    }
+
+}
---
+++ Assets/Content/Script/UI/PreStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreStart : MonoBehaviour
+{
+    public bool canStart = false;
+
+    void Update()
+    {
+        if (canStart && Input.anyKeyDown) {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
