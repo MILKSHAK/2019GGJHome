4d1fed4692391b919680c2dd3358f6b6cc339b61
WeAthFolD <weathfold@li-dev.cn>
2019-01-27 10:59:14 +0800

    Progress bar

--- Assets/Content/Script/Actor/EndingPoint.cs
+++ Assets/Content/Script/Actor/EndingPoint.cs
@@ -7,17 +7,22 @@ public class EndingPoint : MonoBehaviour
     PlayerController _player;
     GameManager _gameManager;
 
+    float _length = 0;
+
     void Start()
     {
         _player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
-        _gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();;
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();;
+        _length = transform.position.x;
     }
 
     bool _gameEnded = false;
 
     void Update()
     {
-        var prog = Mathf.Clamp(_player.transform.position.x / transform.position.x, 0, 1);
+        if (!_player)
+            return;
+        var prog = Mathf.Clamp(1 - transform.position.x / _length, 0, 1);
         _gameManager.gameProgress = prog;
         if (prog == 1 && !_gameEnded) {
             _gameEnded = true;
---
+++ Assets/Content/Script/UI/UIProgress.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIProgress : MonoBehaviour
+{
+    GameManager _gameManager;
+
+    public RectTransform earth;
+
+    public Text text;
+
+    void Start()
+    {
+		_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+        var p = (int) (100 * _gameManager.gameProgress);        
+        earth.anchoredPosition = new Vector2(680.0f * p / 100, 0);
+        text.text = p.ToString() + "%";
+    }
+}
