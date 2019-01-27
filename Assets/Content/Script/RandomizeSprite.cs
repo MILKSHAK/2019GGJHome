26b231b40aa3f77dde0e8981a7ed062d14d86636
WeAthFolD <weathfold@li-dev.cn>
2019-01-27 11:24:12 +0800

    Coronal

---
+++ Assets/Content/Script/RandomizeSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomizeSprite : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    SpriteRenderer _sr;
+
+    void Awake()
+    {
+        _sr = GetComponent<SpriteRenderer>();        
+        _sr.sprite = sprites[Random.Range(0, sprites.Length)];
+        Destroy(this);
+    }
+
+}
